97e354ca24fe70ebe3f4281c9eaef4a0331edc2a
Bibin Jaimon <bibinjaimon@gmail.com>
2019-06-20 12:28:11 +0530

    worked on local offpc

--- name-age-ethereum/server.js
+++ name-age-ethereum/server.js
@@ -3,26 +3,82 @@ const express = require('express');
 var Tx = require('ethereumjs-tx');
 var app = express()
 
-var url = '';
+var url = 'http://127.0.0.1:7545';
 
 var rpcUrl = ''
 
 const web3 = new Web3(new Web3.providers.HttpProvider(url));
 
-const account1 = '';
+const account1 = '0x3833829245E3271b19464bF3B4B2F435b234C844';
 
-const privateKey = ''
+const privateKey = process.env.PRIVATE_KEY_1
 
 const privateKey1 = Buffer.from(privateKey, 'hex')
 
 web3.eth.defaultAccount =  account1
 
-const ABI = [{"constant":false,"inputs":[{"name":"_fName","type":"string"},{"name":"_age","type":"uint256"}],"name":"setInstructor","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"anonymous":false,"inputs":[{"indexed":false,"name":"name","type":"string"},{"indexed":false,"name":"age","type":"uint256"}],"name":"Instructor","type":"event"},{"constant":true,"inputs":[],"name":"getInstructor","outputs":[{"name":"","type":"string"},{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"}]
-
-const contractAddr = '0x3afdb4caa52d4720e900599c5566043c6a2f0c0b'
+const ABI = [
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "_fName",
+				"type": "string"
+			},
+			{
+				"name": "_age",
+				"type": "uint256"
+			}
+		],
+		"name": "setInstructor",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"name": "name",
+				"type": "string"
+			},
+			{
+				"indexed": false,
+				"name": "age",
+				"type": "uint256"
+			}
+		],
+		"name": "Instructor",
+		"type": "event"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "getInstructor",
+		"outputs": [
+			{
+				"name": "",
+				"type": "string"
+			},
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	}
+]
+const contractAddr = '0x18d0bb82ff7e51ee17d4db9b58121bd5df7475d0'
 
 const contract = new web3.eth.Contract(ABI, contractAddr)
-
+app.use(function(req, res, next){
+	res.header("Access-Control-Allow-Origin", "*");
+	next();
+})
 app.get('/set/:name/:age', (req, res) => {
 	const name = req.params.name
 	const age = req.params.age
@@ -32,6 +88,10 @@ app.get('/set/:name/:age', (req, res) => {
 app.get('/fetchData', async (req, res) => {
 	var response = await getInstructorDatas()
 	res.send(response);
+});
+
+app.get('/', (req, res) => {
+	res.sendFile(__dirname + '/index.html');
 });
 
 function setInstructorName(name, age, res) {
@@ -80,6 +140,6 @@ async function getInstructorDatas() {
 	return responseData
 }
 
-app.listen(8080, () => {
-	console.log('Web3 in 8000');
+app.listen(8085, () => {
+	console.log('Web3 in 8080');
 })
